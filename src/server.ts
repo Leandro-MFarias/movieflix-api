b100d498d395e9e8716763c5eaaa9357e683e33d
Leandro Farias <leandrorf1606@gmail.com>
2024-10-14 13:50:29 -0300

    adicionada correção de filmes duplicados

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express"
 import { PrismaClient } from "@prisma/client"
 
+
 const port = 3000
 const app = express()
 const prisma = new PrismaClient()
@@ -24,6 +25,18 @@ app.post("/movies", async (req, res) => {
     const { title, genre_id, language_id, oscar_count, release_date } = req.body
 
     try {
+        // Case insensitive - se a busca for feita por john wick ou John Wick ou JOHN WICK, o registro vai ser retornado na consulta
+
+        // Case sensitive - se buscar por john wick e no banco estiver como John Wick, não vai ser retornado na consulta
+
+        const movieWithSameTitle = await prisma.movie.findFirst({
+            where: { title: {equals: title, mode: "insensitive"} }
+        })
+
+        if(movieWithSameTitle) {
+            return res.status(409).send({message: "Já existe um filme cadastrado com esse título"})
+        }
+
         await prisma.movie.create({
             data: {
                 title: title,
