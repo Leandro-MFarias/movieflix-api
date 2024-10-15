d7055611a35da9fc78e5c579b1d6e638cf95e8d8
Leandro Farias <leandrorf1606@gmail.com>
2024-10-15 18:12:39 -0300

    adicionada a remoção de filmes

--- src/server.ts
+++ src/server.ts
@@ -30,11 +30,11 @@ app.post("/movies", async (req, res) => {
         // Case sensitive - se buscar por john wick e no banco estiver como John Wick, não vai ser retornado na consulta
 
         const movieWithSameTitle = await prisma.movie.findFirst({
-            where: { title: {equals: title, mode: "insensitive"} }
+            where: { title: { equals: title, mode: "insensitive" } }
         })
 
-        if(movieWithSameTitle) {
-            res.status(409).send({message: "Já existe um filme cadastrado com esse título"})
+        if (movieWithSameTitle) {
+            res.status(409).send({ message: "Já existe um filme cadastrado com esse título" })
         }
 
         await prisma.movie.create({
@@ -47,7 +47,7 @@ app.post("/movies", async (req, res) => {
             }
         })
     } catch (error) {
-        res.status(500).send({message: "Falha ao cadastrar um filme"})
+        res.status(500).send({ message: "Falha ao cadastrar um filme" })
     }
 
     res.status(201).json()
@@ -58,29 +58,45 @@ app.put('/movies/:id', async (req, res) => {
     const id = Number(req.params.id)
 
     try {
-    const movie = await prisma.movie.findUnique({
-        where: {
-            id
+        const movie = await prisma.movie.findUnique({
+            where: {
+                id
+            }
+        })
+        if (!movie) {
+            res.status(404).send({ message: "Filme não encontrado" })
         }
-    })
-    if(!movie) {
-        res.status(404).send({ message: "Filme não encontrado" }) 
-    }
 
-    const data = { ...req.body }
-    data.release_date = data.release_date ? new Date(data.release_date) : undefined
+        const data = { ...req.body }
+        data.release_date = data.release_date ? new Date(data.release_date) : undefined
 
-    // pegar os dados do filme que será atualizado e atualizar ele no prisma
-    await prisma.movie.update({
-        where: {
-            id
-        },
-        data: data
-    })
-    }catch (error) {
+        // pegar os dados do filme que será atualizado e atualizar ele no prisma
+        await prisma.movie.update({
+            where: {
+                id
+            },
+            data: data
+        })
+    } catch (error) {
         res.status(500).send({ message: "Falha ao atualizar o registo do filme" })
     }
     // retornar o status correto informando que o filme foi atualizado
+    res.status(200).send()
+})
+
+app.delete('/movies/:id', async (req, res) => {
+    const id = Number(req.params.id)
+
+    try {
+        const movie = await prisma.movie.findUnique({ where: { id } })
+        if (!movie) {
+            res.status(404).send({ message: "O Filme não foi encontrado" })
+        }
+
+        await prisma.movie.delete({ where: { id } })
+    } catch (error) {
+        res.status(500).send({ message: "Não foi possível remover o filme" })
+    }
     res.status(200).send()
 })
 
